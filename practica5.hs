1cf1b40f59a8c33f247d6e0fc40c0a2d3195d0a2
Agustin Nadim Neme <96502841+agusneme@users.noreply.github.com>
2023-09-11 12:56:19 -0300

    Agrego ejercicio maximo

--- practica5.hs
+++ practica5.hs
@@ -1,10 +1,23 @@
+import Distribution.Simple.Utils (xargs)
  -- Ejercicio 1 
 longitud :: [t] -> Integer
 longitud [] = 0
 longitud (_:xs) = 1 + longitud xs
 
--- Ejercicio 4
+-- Ejercicio 3 
+-- 3
+{-
+maximo :: Ord t => [t] -> t
+maximo (x:xs) = maximum (x:xs) 
+-}
 
+maximo :: [Int] -> Int
+maximo [x] = x
+maximo (x:y:xs)  | x > y = maximo (x:xs)
+    | otherwise = maximo (y:xs)
+
+-- Ejercicio 4
 hayRepetidos :: Eq t => [t] -> Bool
 hayRepetidos [] = False
 hayRepetidos (x:xs) = elem x xs || hayRepetidos xs
+
