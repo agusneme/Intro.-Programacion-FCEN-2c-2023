b1d31bd8567500c753a192d5389f998f7fa140c2
Agustin Nadim Neme <96502841+agusneme@users.noreply.github.com>
2023-09-11 13:32:26 -0300

    Agrego ejercicio 3) 9) ordenar

--- practica5.hs
+++ practica5.hs
@@ -15,6 +15,20 @@ maximo :: [Int] -> Int
 maximo [x] = x
 maximo (x:y:xs)  | x > y = maximo (x:xs)
     | otherwise = maximo (y:xs)
+-- 9
+
+ordenar :: [Int] -> [Int]
+ordenar [] = []
+ordenar (x:xs) = minimo xs : ordenar (quitar (minimo xs) xs) 
+
+quitar :: Int -> [Int] -> [Int]
+quitar e (x:xs) | e /= x = x : (quitar e xs)
+    | otherwise = xs
+
+minimo :: [Int] -> Int
+minimo [x] = x
+minimo (x:y:xs)  | x < y = minimo (x:xs)
+    | otherwise = minimo (y:xs)
 
 -- Ejercicio 4
 hayRepetidos :: Eq t => [t] -> Bool
