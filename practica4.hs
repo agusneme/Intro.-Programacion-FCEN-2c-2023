bc3fcc716746d37c9a08dd2f1fccabb109c3b3a0
Agustin Nadim Neme <96502841+agusneme@users.noreply.github.com>
2023-09-11 10:01:19 -0300

    Agrego mas ejercicio de la practica 4

--- practica4.hs
+++ practica4.hs
@@ -14,6 +14,38 @@ parteEntera x
     | x >= 1 = 1 + parteEntera(x-1)
     | otherwise = (-1) + parteEntera(x+1)
 
+-- Ejercicio 3
+
+esDivisible :: Integer -> Integer -> Bool
+esDivisible _ 0 = False
+esDivisible 0 _ = True
+esDivisible x y 
+    | x < y = False
+    | otherwise = esDivisible (x-y) y
+
+-- Ejercicio 4
+
+sumaImpares :: Integer -> Integer
+sumaImpares n = sumaImparesAux  n 1 -1
+
+sumaImparesAux :: Integer -> Integer -> Integer
+sumaImparesAux 0 _ = 0
+sumaImparesAux n impar = impar + sumaImparesAux (n-1) (n+2)
+
+
+-- Ejercicio 5
+
+medioFact :: Integer -> Integer
+medioFact 0 = 1
+medioFact 1 = 1 
+medioFact n = n * medioFact (n-2)
+
+-- Ejercicio 6
+
+sumaDigitos :: Integer -> Integer
+sumaDigitos n
+    | n < 10 = n
+    | otherwise = ultimoDigito n + sumaDigitos (sacarUltimoDigito n)
 
 -- Ejercicio 7
 
@@ -28,5 +60,25 @@ ultimoDigito x = mod x 10
 
 sacarUltimoDigito :: Integer -> Integer
 sacarUltimoDigito x = div x 10
+-- Ejercicio 13 ^^^
+{-
+sumatoriaDoble ::  Integer -> Integer -> Integer
+sumatoriaDoble _ 0 = 0
+sumatoriaDoble n j = n^2 + sumatoriaDoble n(m-1)
+-}
+
+sumaBaseFija :: Integer -> Integer -> Integer
+sumaBaseFija b 1 = b
+sumaBaseFija b e = sumaBaseFija b (e-1) + b ^ e
+
+sumarPotencias :: Integer -> Integer -> Integer
+sumarPotencias 1 m = sumaBaseFija 1 m
+sumarPotencias n m = sumaBaseFija n m + sumarPotencias (n-1) m
 
 
+-- Ejercicio 16
+
+menorDivisor :: Integer -> Integer
+menorDivisor 
+    | x < 1 = True
+    | otherwise = 
