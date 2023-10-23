6c9052c02367e32ce86d73f5d263ece9546af379
Agustin Nadim Neme <96502841+agusneme@users.noreply.github.com>
2023-10-23 11:06:42 -0300

    update

--- Python/guia8_seccion_pilas.py
+++ Python/guia8_seccion_pilas.py
@@ -1,13 +1,26 @@
 from queue import LifoQueue
 
-def buscar_maximo(pila: LifoQueue[int]):
+def copiar(p: LifoQueue) -> LifoQueue:
+    elementos: [int] = []
+    while not p.empty():
+        elementos.append(p.get())
+    p_copy: LifoQueue = LifoQueue()
     
-    maximo = 0
-    for valor in pila.get():       
-       if valor > maximo:
-           maximo = valor
+    for i in range(len(elementos)-1, 0-1,-1):
         
-    return maximo 
+        p.put(elementos[i])
+        p_copy.put(elementos[i])
+    return p_copy
+
+        
+def buscar_maximo(pila):
+    p_copy: LifoQueue[int] = copiar(pila)
+    value = pila.get()
+    while not p_copy.empty():
+       next_value =  p_copy.get()
+       value = max(next_value, value)
+    return value
+
 
 pila = LifoQueue()
 pila.put(3)
@@ -16,5 +29,5 @@ pila.put(1)
 pila.put(22)
 pila.put(300)
 
-
+print(pila.queue)
 print(buscar_maximo(pila))
